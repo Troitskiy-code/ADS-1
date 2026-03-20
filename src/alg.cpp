2b5b43e82bf22ed7dd2da130a47b335fc4efb5f3
Troitskiy-code <mrcheleng87@gmail.com>
2026-03-20 20:48:27 +0300

    Update alg3.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,57 +3,50 @@
 #include "alg.h"
 
 bool checkPrime(uint64_t value) {
-    if (value <= 1) {
-        return false;
-    }
-    if (value > 2 && value % 2 == 0) {
-        return false;
-    }
-    for (uint64_t i = 3; i <= value / 2; i += 2) {
-        if (value % i == 0) {
-            return false;
-        }
+    if (value <= 1) return false;
+    if (value == 2) return true;
+    if (value % 2 == 0) return false;
+    
+    for (uint64_t i = 3; i * i <= value; i += 2) {
+        if (value % i == 0) return false;
     }
     return true;
 }
+
 uint64_t nPrime(uint64_t n) {
-    if (n == 0) {
-        return 0;
-    }
+    if (n == 0) return 0;
+    
     uint64_t count = 0;
-    uint64_t number = 2;
-    while (true) {
-        if (checkPrime(number)) {
-            count++;
-            if (count == n) {
-                return number;
-            }
+    uint64_t num = 2;
+    
+    while (count < n) {
+        if (checkPrime(num)) {
+            ++count;
+            if (count == n) return num;
         }
-        number++;
+        ++num;
     }
+    return 0;
 }
+
 uint64_t nextPrime(uint64_t value) {
-    uint64_t candidate = value + 1;
-    while (!checkPrime(candidate)) {
-        candidate++;
-    }
-    return candidate;
+    uint64_t num = value + 1;
+    while (!checkPrime(num)) ++num;
+    return num;
 }
+
 uint64_t sumPrime(uint64_t hbound) {
     uint64_t sum = 0;
-    for (uint64_t i = 2; i < hbound; i++) {
-        if (checkPrime(i)) {
-            sum += i;
-        }
+    for (uint64_t i = 2; i < hbound; ++i) {
+        if (checkPrime(i)) sum += i;
     }
     return sum;
 }
+
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-    uint64_t cnt = 0;
-    for (uint64_t p = lbound; p <= hbound - 2; p++) {
-        if (checkPrime(p) && checkPrime(p + 2)) {
-            cnt++;
-        }
+    uint64_t count = 0;
+    for (uint64_t i = lbound; i + 2 < hbound; ++i) {
+        if (checkPrime(i) && checkPrime(i + 2)) ++count;
     }
-    return cnt;
+    return count;
 }
